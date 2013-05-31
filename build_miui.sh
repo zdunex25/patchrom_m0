0a63a15c464cb88733777ac955944d171582b5de
zdunex25 <zdunex25@gmail.com>
2013-05-31 11:58:24 +0200

    update of miui v5

--- build_miui.sh
+++ build_miui.sh
@@ -93,6 +93,9 @@ grep -v '<category android:name="android.intent.category.LAUNCHER" />' 'XiaomiSe
 cd ..
 rm -r "temp"
 make fullota
+
+if [ -f "out/fullota.zip" ];
+then
 unzip -q out/fullota.zip -d out/temp
 echo -e "\nPreparing flashable zips.."
 
@@ -140,6 +143,152 @@ rm -r 'META-INF/CERT.RSA'
 rm -r 'META-INF/CERT.SF'
 rm -r 'META-INF/MANIFEST.MF'
 zip -q -r "../../unsigned-miuigalaxy-v5-i9300-$version.zip" 'data' 'META-INF' 'system'
+cp -f ../../other/updater-script META-INF/com/google/android/updater-script
+rm -f system/CSCVersion.txt
+rm -f system/default.prop
+rm -f system/SW_Configuration.xml
+rm -rf system/app/mcRegistry
+rm -r system/app/FFFFFFFF000000000000000000000001.drbin
+rm -r system/app/ApplicationsProvider.apk
+rm -r system/app/BackupRestoreConfirmation.apk
+rm -r system/app/BadgeProvider.apk
+rm -r system/app/BluetoothAvrcp.apk
+rm -r system/app/BluetoothMap.apk
+rm -r system/app/BluetoothTest.apk
+rm -r system/app/Camera.apk
+rm -r system/app/CertInstaller.apk
+rm -r system/app/ClipboardSaveService.apk
+rm -r system/app/CSC.apk
+rm -r system/app/DSPManager.apk
+rm -r system/app/EdmVpnServices.apk
+rm -r system/app/FaceLock.apk
+rm -r system/app/FactoryTest.apk
+rm -r system/app/FmRadio.apk
+rm -r system/app/Gmail.apk
+rm -r system/app/GoogleBackupTransport.apk
+rm -r system/app/GoogleCalendarSyncAdapter.apk
+rm -r system/app/GoogleContactsSyncAdapter.apk
+rm -r system/app/GoogleFeedback.apk
+rm -r system/app/GoogleGMS.apk
+rm -r system/app/GoogleLoginService.apk
+rm -r system/app/GooglePartnerSetup.apk
+rm -r system/app/GoogleServicesFramework.apk
+rm -r system/app/GoogleTTS.apk
+rm -r system/app/KeyChain.apk
+rm -r system/app/LogsProvider.apk
+rm -r system/app/Maps.apk
+rm -r system/app/MediaUploader.apk
+rm -r system/app/minimode-res.apk
+rm -r system/app/MiuiWeather.apk
+rm -r system/app/MtpApplication.apk
+rm -r system/app/MusicFX.apk
+rm -r system/app/Nfc.apk
+rm -r system/app/NfcTest.apk
+rm -r system/app/OmaCP.apk
+rm -r system/app/OriginalSettings.apk
+rm -r system/app/OSB.apk
+rm -r system/app/PanningTryActually.apk
+rm -r system/app/PCWClientS.apk
+rm -r system/app/PCWClientS.apk
+rm -r system/app/PhoneErrService.apk
+rm -r system/app/Phonesky.apk
+rm -r system/app/PopupuiReceiver.apk
+rm -r system/app/Preconfig.apk
+rm -r system/app/PreloadInstaller.apk
+rm -r system/app/PRUI.apk
+rm -r system/app/SamsungIME.apk
+rm -r system/app/SamsungTTS.apk
+rm -r system/app/SecHTMLViewer.apk
+rm -r system/app/SecLiveWallpapersPicker.apk
+rm -r system/app/SecMediaProvider.apk
+rm -r system/app/SecurityProvider.apk
+rm -r system/app/SelfTestMode.apk
+rm -r system/app/serviceModeApp.apk
+rm -r system/app/SPN.apk
+rm -r system/app/Talk.apk
+rm -r system/app/Talkback.apk
+rm -r system/app/UserDictionaryProvider.apk
+rm -r system/app/Velvet.apk
+rm -r system/app/VoiceSearchStub.apk
+rm -r system/app/WAPPushManager.apk
+rm -r system/app/WlanTest.apk
+rm -r system/app/wssyncmlnps.apk
+rm -r system/app/YouTube.apk
+rm -rf system/bin
+cp -rf ../../../miui/XHDPI/system/bin system
+rm -rf system/cameradata
+rm -rf system/csc
+rm -rf system/etc
+cp -rf ../../../miui/XHDPI/system/etc system
+rm -rf system/etc/license
+rm -f system/etc/weather_city.db
+rm -rf system/fonts
+rm -f system/framework/access.control.jar
+rm -f system/framework/allshare.jar
+rm -f system/framework/allsharelib.jar
+rm -f system/framework/am.jar
+rm -f system/framework/android.test.runner.jar
+rm -f system/framework/apache-xml.jar
+rm -f system/framework/bmgr.jar
+rm -f system/framework/bouncycastle.jar
+rm -f system/framework/bu.jar
+rm -f system/framework/com.android.future.usb.accessory.jar
+rm -f system/framework/com.android.location.provider.jar
+rm -f system/framework/com.android.nfc_extras.jar
+rm -f system/framework/com.dropboxpartner.jar
+rm -f system/framework/com.google.android.maps.jar
+rm -f system/framework/com.google.android.media.effects.jar
+rm -f system/framework/com.google.widevine.software.drm.jar
+rm -f system/framework/com.samsung.device.jar
+rm -f system/framework/content.jar
+rm -f system/framework/core.jar
+rm -f system/framework/core-junit.jar
+rm -f system/framework/CustFeature.jar
+rm -f system/framework/ext.jar
+rm -f system/framework/GlobalConfig.jar
+rm -f system/framework/ime.jar
+rm -f system/framework/input.jar
+rm -f system/framework/javax.obex.jar
+rm -f system/framework/kies_feature.jar
+rm -f system/framework/libvtmanagerjar.jar
+rm -f system/framework/minimode.jar
+rm -f system/framework/monkey.jar
+rm -f system/framework/org.simalliance.openmobileapi.jar
+rm -f system/framework/pm.jar
+rm -f system/framework/requestsync.jar
+rm -f system/framework/seccamera.jar
+rm -f system/framework/sec_edm.jar
+rm -f system/framework/secface.jar
+rm -f system/framework/sec_feature.jar
+rm -f system/framework/sechardware.jar
+rm -f system/framework/secmediarecorder.jar
+rm -f system/framework/sec_platform_library.jar
+rm -f system/framework/svc.jar
+rm -f system/framework/twframework.jar
+rm -f system/framework/twframework-res.apk
+rm -f system/framework/uiautomator.jar
+rm -rf system/hdic
+rm -rf system/lib
+cp -rf ../../../miui/XHDPI/system/lib system
+rm -rf system/media/audio
+rm -rf system/media/lockscreen
+rm -rf system/media/wallpaper
+rm -rf system/media/video
+rm -f system/media/*.emd
+rm -f system/media/*.qmg
+rm -f system/media/*.zip
+rm -rf system/media/theme/miui_mod_icons
+rm -f system/media/theme/simple_lockscreen.mtz
+rm -rf system/T9DB
+rm -rf system/tts
+rm -rf system/usr
+rm -rf system/vendor
+rm -rf system/voicebargeindata
+rm -rf system/vsc
+rm -rf system/wakeupdata
+rm -rf system/xbin
+cp -rf ../../../miui/XHDPI/system/xbin system
+zip -q -r "../../unsigned-miuigalaxy-v5-i9300-ota-to-$version.zip" 'META-INF' 'system'
 cd ../..
 . ../build/envsetup.sh
 cd m0
@@ -152,9 +301,13 @@ rm -rf 'Settings/res/xml'
 rm -rf 'Settings/smali'
 rm -rf 'XiaomiServiceFramework'
 make clean
-echo Signing rom
+echo Signing rom and ota
 java -jar 'other/signapk.jar' 'other/testkey.x509.pem' 'other/testkey.pk8' "unsigned-miuigalaxy-v5-i9300-$version.zip" "miuigalaxy-v5-i9300-$version.zip"
 rm -r "unsigned-miuigalaxy-v5-i9300-$version.zip"
-echo -e "MD5 sum is\n"
+java -jar 'other/signapk.jar' 'other/testkey.x509.pem' 'other/testkey.pk8' "unsigned-miuigalaxy-v5-i9300-ota-to-$version.zip" "miuigalaxy-v5-i9300-ota-to-$version.zip"
+rm -r "unsigned-miuigalaxy-v5-i9300-ota-to-$version.zip"
+echo -e "MD5 sums are\n"
 md5sum -b "miuigalaxy-v5-i9300-$version.zip"
-read -p "Done, miuigalaxy-v5-i9300-$version.zip and patch have been created in root of m0 directory, copy to sd and flash it!"
+md5sum -b "miuigalaxy-v5-i9300-ota-to-$version.zip"
+fi
+read -p "Done, miuigalaxy-v5-i9300-$version.zip and OTA have been created in root of m0 directory, copy to sd and flash it!"
